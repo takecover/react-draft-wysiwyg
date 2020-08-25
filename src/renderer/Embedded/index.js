dc9ed4b24124b88f4c317794424c6b6d2f5ebed8
takecover <takecover@gmail.com>
2020-08-25 13:40:31 +0900

    16:9 div container added which wraps Embedded

--- src/renderer/Embedded/index.js
+++ src/renderer/Embedded/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Embed = ({ block, contentState }) => {
   const entity = contentState.getEntity(block.getEntityAt(0));
   const { src, height, width } = entity.getData();
-  return (<iframe height={height} width={width} src={src} frameBorder="0" allowFullScreen title="Wysiwyg Embedded Content" />);
+  return (<div style="position: relative; width: 100%; padding-top: 56.25%;"><iframe height={height} width={width} src={src} frameBorder="0" allowFullScreen title="Wysiwyg Embedded Content" /></div>);
 };
 
 Embed.propTypes = {
