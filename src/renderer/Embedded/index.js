18088d676de0278288244d3f310685b3c95fcdbc
takecover <takecover@gmail.com>
2020-08-25 14:03:02 +0900

    embedded container and iframe configured

--- src/renderer/Embedded/index.js
+++ src/renderer/Embedded/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.css';
 
 const Embed = ({ block, contentState }) => {
   const entity = contentState.getEntity(block.getEntityAt(0));
   const { src, height, width } = entity.getData();
-  return (<div style="position: relative; width: 100%; padding-top: 56.25%;"><iframe height={height} width={width} src={src} frameBorder="0" allowFullScreen title="Wysiwyg Embedded Content" /></div>);
+  return (
+    <div className="rdw-embedded-container">
+      <iframe className="rdw-embedded-iframe" height={height} width={width} src={src} frameBorder="0" allowFullScreen title="Wysiwyg Embedded Content" />
+    </div>
+  );
 };
 
 Embed.propTypes = {
